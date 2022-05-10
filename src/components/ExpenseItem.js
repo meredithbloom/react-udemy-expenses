3b040b2fdcf644bebea17a3b332b4ec10f0fbbdd
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-05-09 21:31:51 -0400

    basic react udemy course expense app file structure

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>Date</div>
+            <div>
+                <h2>Expense Item!</h2>
+            </div>
+        </div>
+        
+        
+    )
+}
+
+export default ExpenseItem
