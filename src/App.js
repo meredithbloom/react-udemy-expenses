42f838a84e7fe6cd1473212364ca7c6f70e3b6e4
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-05-11 19:51:32 -0400

    finished first react udemy project - simple expense tracker

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import ExpensesFilter from './components/Expenses/ExpensesFilter';
 
 
 const DUMMY_EXPENSES = [
-    { id: 'e1', title: 'Equinox', amount: 185, date: new Date(2022, 5, 23) },
+    { id: 'e1', title: 'Equinox', amount: 185, date: new Date(2022, 3, 23) },
     { id: 'e2', title: 'Boxing', amount: 99.99, date: new Date(2020, 5, 10) },
     { id: 'e3', title: 'Plane Ticket', amount: 300, date: new Date(2020, 6, 10) },
-    { id: 'e4', title: 'Medicine', amount: 100, date: new Date(2021, 5, 6) }
+    { id: 'e4', title: 'Medicine', amount: 100, date: new Date(2021, 10, 6) }
   ]
 
 
